2021e1053237d9d09d29cd2ce2221e06d3928c27
Divish <divish_28@hotmail.com>
2023-10-03 16:05:21 -0400

    Add functions for download separate audio, video, merging

--- backend/src/utils/validURL.ts
+++ backend/src/utils/validURL.ts
@@ -1,29 +1,6 @@
 import ytdl from "ytdl-core";
-import fs from "node:fs"; // more correct way
-
-//http for downloading
-
-/**
- * Validates a URL to check if it has the "https" protocol, a non-empty host, and a non-empty pathname.
- * @param {string} url - The URL to validate.
- * @returns {boolean} - True if the URL is valid; otherwise, false.
- */
-function validateURL(url: string): boolean {
-    try {
-      const testURL = new URL(url);
-  
-      // Check the protocol, host, and pathname
-      const isValid =
-        testURL.protocol === "https:" &&
-        testURL.host !== "" &&
-        testURL.pathname !== "";
-  
-      return isValid;
-    } catch (error) {
-      console.error("Error while validating URL:", error);
-      return false; // URL is not parseable
-    }
-  }
+import ffmpeg from "fluent-ffmpeg";
+import fs from "node:fs";
 
 /**
  * Validates a URL to check if it is a valid YouTube video URL.
@@ -31,17 +8,18 @@ function validateURL(url: string): boolean {
  * @returns {boolean} - True if the URL is a valid YouTube video URL; otherwise, false.
  */
 export const validateYouTubeURL = (url: string): boolean => {
-    try {
-      // todo add regex for YouTube shorts too
-      const youtubeUrlPattern = /^(https?:\/\/)?(www\.)?youtube\.com\/watch\?v=[\w-]+(&\S*)?$/;
-      const isValid = youtubeUrlPattern.test(url);
-  
-      return isValid;
-    } catch (error) {
-      console.error("Error while validating YouTube URL:", error);
-      return false; // URL is not parseable
-    }
-}
+  try {
+    // todo add regex for YouTube shorts too
+    const youtubeUrlPattern =
+      /^(https?:\/\/)?(www\.)?youtube\.com\/watch\?v=[\w-]+(&\S*)?$/;
+    const isValid = youtubeUrlPattern.test(url);
+
+    return isValid;
+  } catch (error) {
+    console.error("Error while validating YouTube URL:", error);
+    return false; // URL is not parseable
+  }
+};
 
 interface VideoInfo {
   title: string;
@@ -53,88 +31,139 @@ interface VideoInfo {
 interface Format {
   mimeType: string | undefined;
   quality: string;
-  size : number;
+  size: number;
 }
 
 export const getVideoInfo = async (url: string): Promise<VideoInfo> => {
-  const info = await ytdl.getInfo(url)
+  const info = await ytdl.getInfo(url);
   const result: VideoInfo = {
     // Lots of empty spaces in YT videos, replace with dash
-    title: info.videoDetails.title,  
-    duration: info.videoDetails.lengthSeconds, 
+    title: info.videoDetails.title,
+    duration: info.videoDetails.lengthSeconds,
     videoId: info.videoDetails.videoId,
     formats: info.formats.map((element) => ({
       mimeType: element.mimeType,
-      quality:element.qualityLabel,
-      size: parseInt(element.contentLength)
-    }))
-
-  } 
+      quality: element.qualityLabel,
+      size: parseInt(element.contentLength),
+    })),
+  };
   return result;
-}
+};
 
-// export const downloadAsAudio = async (url:string, title:string) => {
-//   // let video = ytdl(url, {filter: "audioonly"});
-//   ytdl(url, {filter: "audioonly"})
-//     .pipe(fs.createWriteStream(`./${title}.m4a`))
-//     .on("finish", () => console.log("Finished downloaded"))
-//     .on("error", (err) => {
-//       console.error(err);
-//     })
-// }
-
-export const downloadFile = async (fileTypeToDownload:string, url:string, title:string) => {
-
+export const downloadFile = async (
+  fileTypeToDownload: string,
+  url: string,
+  title: string
+) => {
   if (fileTypeToDownload === "audio-m4a") {
-    ytdl(url, {filter: "audioonly"})
+    ytdl(url, { filter: "audioonly" })
       .pipe(fs.createWriteStream(`./${title}.m4a`))
       .on("finish", () => console.log("Finished downloaded"))
       .on("error", (err) => {
         console.error(err);
-      })
+      });
   }
 
   if (fileTypeToDownload === "360p") {
-    ytdl.getInfo(url)
-      .then((info) => {
-        // const format = ytdl.chooseFormat(info.formats, {quality: "18"})
-        const choosenFormat = info.formats.find(format => format.itag === 18);
-        console.log(choosenFormat);
-        const videoStream = ytdl(url, {
-          format: choosenFormat
-        });
-        videoStream.pipe(fs.createWriteStream("./360p-video.mp4"));
-      })
+    ytdl.getInfo(url).then((info) => {
+      // const format = ytdl.chooseFormat(info.formats, {quality: "18"})
+      // todo itag 137 is h.264 codec, 248 is vp9 codec
+      // https://imagekit.io/blog/vp9-vs-h264/   (site says vp9 better vodec)
+      // maybe use ffmpeg setttings to convert to vp9 codec
+      // use h.264 codec because more widespread support
+      // research and compare to av1
+      const choosenFormat = info.formats.find((format) => format.itag === 137);
+      console.log(choosenFormat);
+      const videoStream = ytdl(url, {
+        format: choosenFormat,
+      });
+      videoStream.pipe(fs.createWriteStream("./360p-video.mp4"));
+    });
   }
 
+  if (fileTypeToDownload === "720p") {
+    console.log("720p clicked!");
+    saveVideoInDirectory(url, title, 136);
+  }
 
+  if (fileTypeToDownload === "1080p") {
+    // await saveVideoInDirectory(url, title, 137);
+    // await saveAudioInDirectory(url, title);
+    await combineAudioVideo(title);
+  }
+};
 
+export const saveVideoInDirectory = async (
+  url: string,
+  title: string,
+  itag: number
+) => {
+  ytdl
+    .getInfo(url)
+    .then((info) => {
+      const choosenFormat = info.formats.find((format) => format.itag === itag);
+      const videoStream = ytdl(url, { format: choosenFormat });
+      videoStream
+        .pipe(fs.createWriteStream(`./downloads/${title}-video.mp4`))
+        .on("error", (err) => console.error(err));
+    })
+    .catch((err) => console.error(err));
+};
 
-}
+export const saveAudioInDirectory = async (url: string, title: string) => {
+  ytdl(url, { filter: "audioonly" })
+    .pipe(fs.createWriteStream(`./downloads/${title}.m4a`))
+    .on("error", (err) => console.error(err));
+};
 
-getVideoInfo("https://www.youtube.com/watch?v=0mCVpUDCkEk&pp=ygUPd2Ugc3RpbGwgcm9sbGlu")
-  .then((videoInfo) => {
-    console.log(videoInfo);
-  })
-  .catch((error) => {
-    console.error("Error:", error);
-  });
+// getVideoInfo("https://www.youtube.com/watch?v=0mCVpUDCkEk&pp=ygUPd2Ugc3RpbGwgcm9sbGlu")
+//   .then((videoInfo) => {
+//     console.log(videoInfo);
+//   })
+//   .catch((error) => {
+//     console.error("Error:", error);
+//   });
 // async function download (url: string): Promise{
 //   const info = await ytdl.getInfo(url)
 //   console.log(info);
 // }
 const fetchVideos = async () => {
-// const info = await ytdl.getInfo("https://www.youtube.com/watch?v=z5uEMhZJCqo&list=RDz5uEMhZJCqo&start_radio=1")
-// const videoFormats = ytdl.filterFormats(info.formats, "videoonly");
-// console.log(videoFormats);
-ytdl.getInfo("https://www.youtube.com/watch?v=0mCVpUDCkEk&pp=ygUPd2Ugc3RpbGwgcm9sbGlu")
+  // const info = await ytdl.getInfo("https://www.youtube.com/watch?v=z5uEMhZJCqo&list=RDz5uEMhZJCqo&start_radio=1")
+  // const videoFormats = ytdl.filterFormats(info.formats, "videoonly");
+  // console.log(videoFormats);
+  ytdl
+    .getInfo(
+      "https://www.youtube.com/watch?v=0mCVpUDCkEk&pp=ygUPd2Ugc3RpbGwgcm9sbGlu"
+    )
     .then((info) => {
       const qualityOptions = ["135"];
-      const format = ytdl.chooseFormat(info.formats,  {quality: "highest"});
-    })
-}
+      const format = ytdl.chooseFormat(info.formats, { quality: "highest" });
+    });
+};
 
-fetchVideos();
+// fetchVideos();
 
 // const url = "https://www.youtube.com/watch?v=0mCVpUDCkEk&pp=ygUPd2Ugc3RpbGwgcm9sbGlu";
 // ytdl(url).pipe(fs.createWriteStream("video.mp4"));
+// Do not need create child process if using fluent-ffmpeg
+export const combineAudioVideo = async (title: string) => {
+  const audioFile = `./downloads/${title}.m4a`;
+  const videoFile = `./downloads/${title}-video.mp4`;
+  const outputVideoFile = `./downloads/${title}-combined.mp4`;
+
+  const command = ffmpeg();
+  command.input(videoFile);
+  command.input(audioFile);
+
+  command
+    .output(outputVideoFile)
+    .on("end", () => console.log("Audio and video have been combined"))
+    .on("error", (err) =>
+      console.error(`Erroring combining audio/video: ${err}`)
+    );
+
+  command.run();
+};
+
+// combineVideos();
+console.log("Non-blocking code should run here....");
