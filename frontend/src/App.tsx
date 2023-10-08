e16f40c13fca1f1b5d6e6027104b46df9f5b357b
Divish Ram <64987750+divishram@users.noreply.github.com>
2023-10-07 20:40:50 -0400

    Update App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,23 +1,16 @@
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 import "./App.css";
 import Nav from "./components/Header";
 import MainPart from "./components/Main";
-import axios from "axios";
 import { socket } from "./socket";
-import Status from "./components/ConnectionState";
 
 function App() {
-  const [isConnected, setIsConnected] = useState(socket.connected);
-  const [fooEvents, setFooEvents] = useState([]);
-
   useEffect(() => {
     function onConnect() {
-      setIsConnected(true);
       console.log("User conencted from React.js application!");
     }
 
     function onDisconnect() {
-      setIsConnected(false);
       console.log("user disconnected from React.js");
     }
 
@@ -27,15 +20,13 @@ function App() {
     return () => {
       socket.off("connect", onConnect);
       socket.off("disconnect", onDisconnect);
-    }
-
+    };
   }, []);
 
   return (
     <>
       <Nav></Nav>
       <MainPart></MainPart>
-      {/* <Status props={isConnected}></Status> */}
     </>
   );
 }
